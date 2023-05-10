821f104d5f1f5d50be81d5bbe1e38e87f0066b7d
Anton James Genabio <jamesgenabio31@gmail.com>
2023-05-10 21:48:32 +0800

    Update (2.7.9) Validation

--- AppsDev/Form3.cs
+++ AppsDev/Form3.cs
@@ -45,23 +45,41 @@ namespace AppsDev
         Form2 getR = new Form2();
 
         private static string path = @"C:\Barangay Registration\Admin\";
+        private List<UserLogin> list = new List<UserLogin>();
         private void signUp_Click(object sender, EventArgs e)
         {
+
+
             if (userIn != "" && passIn != "")
             {
-                if (passIn.Equals(confirm))
+                int count;
+                for (count = 0; count < File.ReadAllLines(path + "Users.txt").Length; count++)
                 {
+                    string[] frag = File.ReadAllText(path + "Users.txt").Split("\n");
+                    string[] us = frag[count].Split(",");
 
-                
-              
+                    UserLogin u = new UserLogin(us[0], us[1], us[2], us[3], us[4], us[5], us[6], us[7], us[8], us[9], us[10]);
+                    list.Add(u);
 
-                    Users admin = new Users(getR.getReference(), userIn, passIn, getR.firstinfo);
-                    string[] lines = File.ReadAllLines(path + "Users.txt");
+                }
 
-                    using (StreamWriter writeData = new StreamWriter(path + "Users.txt"))
+
+                UserLogin? user = list.Where(UserLogin => UserLogin.username.Equals(userIn)).FirstOrDefault();
+                if (user == null)
+                {
+                    if (passIn.Equals(confirm))
+                    {
+
+
+
+
+                        Users admin = new Users(getR.getReference(), userIn, passIn, getR.firstinfo);
+                        string[] lines = File.ReadAllLines(path + "Users.txt");
+
+                        using (StreamWriter writeData = new StreamWriter(path + "Users.txt"))
                         {
 
-                            
+
                             for (int i = 0; i < lines.Length; i++)
                             {
                                 writeData.WriteLine(lines[i]);
@@ -71,20 +89,25 @@ namespace AppsDev
 
 
 
-                    
-                
-               
-                    
 
 
-                    Form4 form4 = new Form4();
-                    form4.Show();
-                    this.Close();
+
+
+
+
+                        Form4 form4 = new Form4();
+                        form4.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Confirm Password");
+
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Invalid Confirm Password");
-
+                    MessageBox.Show("Username already exist!");
                 }
             }
             else
